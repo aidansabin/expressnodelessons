9e210819f5b583968df8e310387a60854a0c6154
Aidan Sabin <aidansabin@icloud.com>
2021-04-29 16:26:01 +0100

    attempted fix #1 after env file
    lesson 6

--- myApp.js
+++ myApp.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/json", function(req, res) {
-  if (process.env.MESSAGE_STYLE === uppercase) {
-    res.json({"message": "HELLO JSON"});
-  } else {
-    res.json({"message": "Hello json"});
+  var messsage = {"message": "Hello json"};
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message.message = message.message.toUpperCase();
   }
+  res.json(message);
 });
 
 
