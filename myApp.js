f71b4e50ba1bd114c678456748d683f3323aa7c4
Aidan Sabin <aidansabin@icloud.com>
2021-04-30 03:04:44 +0100

    parse post requests
    lesson 11

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   var log = req.method + ' ' + req.path + ' - ' + req.ip;
